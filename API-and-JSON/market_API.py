3e201845c1e791dea4dc5233249fefbd41759063
Công Luân <luannguyenhaha7@gmail.com>
2026-09-12 18:48:05 +0000

    Viết script gọi API của Alpha Vantage để lấy giá cổ phiếu

--- API-and-JSON/market_API.py
+++ API-and-JSON/market_API.py
@@ -1,4 +1,22 @@
 #Import module 
+from requests import Response
 import os
 import sys
 import requests
+
+Base_url = "https://www.alphavantage.co/query"
+
+def get_daily_series(ma, api_key):
+    params = {
+        "function": "TIME_SERIES_DAILY",
+        "symbol": ma,
+        "apikey": api_key
+    }
+
+    Response = requests.get(Base_url, params = params, timeout=15)
+    Response.raise_for_status()
+    return Response.json()
+
+def main():
+    
+    
